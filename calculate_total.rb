6f0d770932170b7b86bd89d776aa1004d0c87913
Jonathan Wilson <jswilson@Jonathans-MacBook-Pro.local>
2014-02-25 19:43:48 -0500

    Initial revision; added calculate_total function and associated tests

---
+++ calculate_total.rb
@@ -0,0 +1,25 @@
+TAX_RATE = 1.05
+
+def calculate_total(order=[])
+	total = 0
+    for item in order
+    	quantity = item[:quantity]
+		unit_price = item[:unit_price]
+		total += quantity * unit_price
+	end
+
+	total *= TAX_RATE
+	return "$%.2f" % total
+end
+
+def calculate_total_var_args(*order)
+	total = 0
+    for item in order
+    	quantity = item[:quantity]
+		unit_price = item[:unit_price]
+		total += quantity * unit_price
+	end
+
+	total *= TAX_RATE
+	return "$%.2f" % total
+end
